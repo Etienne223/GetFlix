5b239b47e5eed1c0d33f67ee5fb2b86c8c4e2493
Jann Basso <jannbasso@Janns-MacBook-Air.local>
2021-03-29 14:43:08 +0200

    include movie form

---
+++ dbconnection.php
@@ -0,0 +1,9 @@
+<?php
+    // connect to database
+    try {
+        $database = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+     }
+     catch(Exception $e) {
+         die('Erreur : '.$e->getMessage());
+     }
+?>
