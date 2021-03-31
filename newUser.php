cb0709826ec83b4f2fb18bde958010042ea3855f
GuillaumeLambermont <61625398+GuillaumeLambermont@users.noreply.github.com>
2021-03-31 13:51:35 +0200

    adding security

--- newUser.php
+++ newUser.php
@@ -13,25 +13,25 @@ if (isset($_POST['pseudo']) && isset($_POST['password']) && isset($_POST['email'
     $authorization = 0; // AUTHORIZATION - DETERMINE SI L'USER EST ADMIN OU NON 0 = FALSE / 1 = TRUE
 
     //CHECK SI EMAIL EXISTE DEJA
-    $bdd = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA BDD
-    $requete = $bdd->prepare("SELECT * FROM users WHERE mail = ?");
+    $db = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA db
+    $requete = $db->prepare("SELECT * FROM users WHERE mail = ?");
     $requete->execute([$email]);
     $resultat = $requete->rowCount();
     if($resultat > 0)
     $error = "Email already taken.";
 
     //CHECK SI USER EXIST DEJA
-    $bdd = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA BDD
-    $requete = $bdd->prepare("SELECT * FROM users WHERE pseudo = ?");
+    $db = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA db
+    $requete = $db->prepare("SELECT * FROM users WHERE pseudo = ?");
     $requete->execute([$nickname]);
     $result = $requete->rowCount();
     if($result > 0)
       $error = "Username already taken";
 
-    //Intégration dans bdd
+    //Intégration dans db
     if(empty($error)){
-      $bdd = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA BDD
-      $requete = $bdd-> prepare('INSERT INTO users(pseudo, password, mail, date_insc, authorization) VALUES(?, ?, ?, ?, ?)'); // INTEGRE LES VALEUR PROVENANT DU FORM DANS LA BDD
+      $db = new PDO('mysql:host=localhost;dbname=getflix', 'root', 'root'); // CONNEXION A LA db
+      $requete = $db-> prepare('INSERT INTO users(pseudo, password, mail, date_insc, authorization) VALUES(?, ?, ?, ?, ?)'); // INTEGRE LES VALEUR PROVENANT DU FORM DANS LA db
       $requete->execute(array($nickname, $hashed_password, $email, $date, $authorization));
 
       header('Location:index.php'); // REDIRIGE VERS LA PAGE DE CONNEXION UNE FOIS QUE L'UTILISATEUR S'EST ENREGISTRE
