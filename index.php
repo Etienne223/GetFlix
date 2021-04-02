4cd218bb320462ba0b4f851f0ffa8d6c258f6e4b
Etienne Lefèvre <etiennelfvre@gmail.com>
2021-04-02 15:57:03 +0200

    Vdd fini huhoha

--- index.php
+++ index.php
@@ -24,7 +24,7 @@
         </div>
         <?php include 'header.php'; ?>
         <main class="boxForm"> 
-            <form id="registrationForm" action="newUser.php" method="POST">
+            <form action="login.php" method="POST">
                 <img src="assets/images/getflix_logo.png" alt="Logo GetFlix">
                 <div>
                     <input class="form__field" type="text" name="pseudo" id="pseudo" placeholder="Name" required>
@@ -39,43 +39,7 @@
                 <a class="account" href="register.php"><p>No account yet ?</p></a>
             </form>
         </main>
-        <footer>
-            <section id="qanda">
-                <h2>Q&A</h2>
-                <ul>
-                    <li>How to get an account ?</li>
-                    <li>Why is it not free ?</li>
-                    <li>Legals</li>
-                </ul>
-            </section>
-            <section id="help">
-                <h2>Help</h2>
-                <ul>
-                    <li>I forgot my password</li>
-                    <li>Terms of use</li>
-                    <li>Contact us</li>
-                </ul>
-            </section>
-            <section id="account">
-                <h2>Account</h2>
-                <ul>
-                    <li>Your account</li>
-                    <li>Payment method</li>
-                    <li>Your favourite movies</li>
-                </ul>
-            </section>
-            <section id="about">
-                <h2>About</h2>
-                <ul>
-                    <li>Who are we ?</li>
-                    <li>Cookies</li>
-                    <li>Share us</li>
-                </ul>
-            </section>
-            <section>
-                <p id="quote" target="_blank">This website was made by the <a href="https://github.com/Etienne223/GetFlix">GETFLIX™ TEAM</a></p>
-            </section>
-        </footer>
+        <?php include 'footer.php'; ?>
     </body>
     <script src="style.js"></script>
     <script src="https://kit.fontawesome.com/f3619f716d.js" crossorigin="anonymous"></script>
