f852055100a6124a73353710397320501921d219
GuillaumeLambermont <61625398+GuillaumeLambermont@users.noreply.github.com>
2021-04-02 08:33:31 +0200

    working login

--- index.php
+++ index.php
@@ -34,15 +34,15 @@
                 <i id="burger" class="fa fa-bars"></i>
             </nav>
         </header>
-        <main class="box"> 
-            <form id="registrationForm" action="newUser.php" method="POST">
+        <main class="box">
+            <form id="registrationForm" action="login.php" method="POST">
                 <img src="assets/images/getflix_logo.png" alt="Logo GetFlix">
                 <div>
                     <input class="form__field" type="text" name="pseudo" id="pseudo" placeholder="Name" required>
                     <label class="form__label" for="pseudo">Enter your pseudo</label>
                 </div>
-                <div> 
-                    <input class="form__field" type="password" name="password" id="password" placeholder="Password" required>                   
+                <div>
+                    <input class="form__field" type="password" name="password" id="password" placeholder="Password" required>
                     <label class="form__label" for="password">Enter your password</label>
                     <a id="lost" class="account" href="lostPassword.php"><p>I forgot my password</p></a>
                 </div>
